881550d249080025744479c5a8b69ce8da4e2d37
雪見だいふく <100012108+mochidukiyukimi@users.noreply.github.com>
2023-07-31 16:23:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -261,7 +261,9 @@ def view_bbs(request: Request,t: str,channel:Union[str,None]="main",verify: Unio
 def write_bbs(request: Request,name: str = "",message: str = "",seed:Union[str,None] = "",channel:Union[str,None]="main",verify:Union[str,None]="false",yuki: Union[str] = Cookie(None)):
     if not(check_cokie(yuki)):
         return redirect("/")
-    requests.get(fr'{url}bbs/result?name={urllib.parse.quote(name)}&message={urllib.parse.quote(message)}&seed={urllib.parse.quote(seed)}&channel={urllib.parse.quote(channel)}&verify={urllib.parse.quote(verify)}&serververify={os.getenv("official", "false")}',cookies={"yuki":"True"})
+    t = requests.get(fr"{url}bbs/result?name={urllib.parse.quote(name)}&message={urllib.parse.quote(message)}&seed={urllib.parse.quote(seed)}&channel={urllib.parse.quote(channel)}&verify={urllib.parse.quote(verify)}&serververify={os.getenv('official', 'false')}",cookies={"yuki":"True"})
+    if t.status_code != 304:
+        return t.text
     return redirect(f"/bbs?name={urllib.parse.quote(name)}&seed={urllib.parse.quote(seed)}&channel={urllib.parse.quote(channel)}&verify={urllib.parse.quote(verify)}")
 
 @cache(seconds=30)
