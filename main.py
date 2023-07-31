1376d1c9c51d160a55cf899272ecf44eeac63069
雪見だいふく <100012108+mochidukiyukimi@users.noreply.github.com>
2023-07-31 13:38:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -259,11 +259,9 @@ def view_bbs(request: Request,t: str,channel:Union[str,None]="main",verify: Unio
 
 app.get("/bbs/result")
 def write_bbs(request: Request,name: str = "",message: str = "",seed:Union[str,None] = "",channel:Union[str,None]="main",verify:Union[str,None]="false",yuki: Union[str] = Cookie(None)):
-    if yuki != "True":
+    if not(check_cokie(yuki)):
         return redirect("/")
     requests.get(fr'{url}bbs/result?name={urllib.parse.quote(name)}&message={urllib.parse.quote(message)}&seed={urllib.parse.quote(seed)}&channel={urllib.parse.quote(channel)}&verify={urllib.parse.quote(verify)}',cookies={"yuki":"True"})
-    if verify == "on":
-        verify = "true"
     return redirect(f"/bbs?name={urllib.parse.quote(name)}&seed={urllib.parse.quote(seed)}&channel={urllib.parse.quote(channel)}&verify={urllib.parse.quote(verify)}&serververify={os.getenv('official', 'false')}")
 
 @cache(seconds=30)
