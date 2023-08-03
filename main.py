8a110eaeff4c1abd563549154d37cc0703744e2a
雪見だいふく <100012108+mochidukiyukimi@users.noreply.github.com>
2023-08-03 09:23:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -262,7 +262,7 @@ def write_bbs(request: Request,name: str = "",message: str = "",seed:Union[str,N
         return redirect("/")
     t = requests.get(fr"{url}bbs/result?name={urllib.parse.quote(name)}&message={urllib.parse.quote(message)}&seed={urllib.parse.quote(seed)}&channel={urllib.parse.quote(channel)}&verify={urllib.parse.quote(verify)}",cookies={"yuki":"True"}, allow_redirects=False)
     if t.status_code != 307:
-        return t.text
+        return HTMLResponse(t.text)
     return redirect(f"/bbs?name={urllib.parse.quote(name)}&seed={urllib.parse.quote(seed)}&channel={urllib.parse.quote(channel)}&verify={urllib.parse.quote(verify)}")
 
 @cache(seconds=30)
